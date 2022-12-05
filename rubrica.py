52cc696e9c1d93ea09e165cf7c9973375776f32a
Salvatore <tore95@icloud.com>
2022-12-05 11:40:01 +0100

    Struttura base del programma

---
+++ rubrica.py
@@ -0,0 +1,53 @@
+# Struttura del progetto
+
+# Il programma è una rubrica che tiene traccia dei nomi, ha diverse funzionalità:
+#   Aggiungere un nome
+#   Modificare un nome
+#   Eliminare un nome
+#   Visualizzare tutta la rubrica
+
+#Implementare funzioni:
+
+def visualizza_lista(lista):
+    return
+
+def aggiungi_nome(lista, nome):
+    return
+
+def elimina_nome(lista, nome):
+    return
+
+def modifica_nome(lista, vecchio_nome, nuovo_nome):
+    return
+
+#Avviamo il programma
+print("Benvenuto nel programma rubrica:")
+print("comandi disponibili: lista, aggiungi, elimina, modifica, esci")
+
+comando = input("inserire comando >> ")
+esci = False
+nomi = []
+
+while not esci:
+
+    match comando:
+        case 'lista':
+            visualizza_lista(nomi)
+        case 'aggiungi':
+            nome = input("Inserisci nome da aggiungere >> ")
+            aggiungi_nome(nomi, nome)
+        case 'elimina':
+            nome = input("Inserisci nome da eliminare >> ")
+            elimina_nome(nomi, nome)
+        case 'modifica':
+            nome = input("Inserisci nome da modificare >> ")
+            nuovo_nome = input("Inserisci il nuovo nome >> ")
+            modifica_nome(nomi, nome, nuovo_nome)
+        case 'esci':
+            print("sto chiudendo il programma")
+            esci = True
+        case _:
+            print("hai inserito un comando sbagliato, riprova")
+    
+    if not esci:
+        comando = input("inserire comando >> ")
